648ffccffd49922763e59c63eb64049d5d22d150
Imron <imron.gamidli@lt.hs-fulda.de>
2021-10-28 15:08:38 +0200

    add all

--- U2/src/nachher/B/BFactory.java
+++ U2/src/nachher/B/BFactory.java
@@ -1,10 +1,14 @@
+//should be changed, rückgabe usw
+
 package nachher.B;
 
 import nachher.A.I_toBFactory;
+import nachher.A.I_to_B1;
 
 public class BFactory implements I_toBFactory {
+	
 	@Override
-	public B1 create(int x) {
+	public I_to_B1 create(int x) {
 		return new B1(x);
 	}
 
